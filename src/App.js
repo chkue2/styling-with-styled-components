5827746d332683955cd1230ecbef3719228b34ab
chkue2 <chkue0924@gmail.com>
2021-04-15 19:14:45 +0900

    create

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled, {ThemeProvider} from 'styled-components';
+import Button from './components/Button';
+import Dialog from './components/Dialog';
+
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+const palette = {
+  blue: '#228be6',
+  gray: '#496057',
+  pink: '#f06595'
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const[dialog, setDialog] = useState(false);
+  const onClick = () => setDialog(true);
+  const onConfirm = () => {
+    setDialog(false);
+    console.log('확인');
+  }
+  const onCancel = () => {
+    setDialog(false);
+    console.log('취소');
+  }
+
+    return(
+      <ThemeProvider theme={{palette}}>
+        <>
+        <AppBlock>
+          <Button color="pink" size="large" onClick={onClick}>
+            삭제
+          </Button>
+        </AppBlock>
+        <Dialog title="정말로 삭제하시겠습니까?" confirmText="삭제" cancelText="취소" visible={dialog} onConfirm={onConfirm} onCancel={onCancel}>
+            데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+        </>
+      </ThemeProvider>
+    );
 }
 
 export default App;
